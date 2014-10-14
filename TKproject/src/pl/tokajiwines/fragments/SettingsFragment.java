d75d517ff5a36fbb984b29c68f76ebfec6b418d9
PSyron <paul.cyron1@gmail.com>
2014-10-14 14:39:43 +0200

    Settings view started + integrating Google Maps Api v2 started

--- TKproject/src/pl/tokajiwines/fragments/SettingsFragment.java
+++ TKproject/src/pl/tokajiwines/fragments/SettingsFragment.java
@@ -1,6 +1,33 @@
 
 package pl.tokajiwines.fragments;
 
-public class SettingsFragment extends BaseFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
 
+import pl.tokajiwines.R;
+
+public class SettingsFragment extends BaseFragment {
+    ListView mUiList;
+
+    public static SettingsFragment newInstance() {
+        SettingsFragment fragment = new SettingsFragment();
+        //        Bundle args = new Bundle();
+        //        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        //        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public SettingsFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+        //        mUiList = (ListView) rootView.findViewById(R.id.frag_news_list);
+
+        return rootView;
+    }
 }
