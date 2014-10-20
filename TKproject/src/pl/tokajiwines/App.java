528f1386dead2a83ebbca5a613c8baf54169a767
paulina00 <konieczna.paulina00@gmail.com>
2014-10-20 19:50:01 +0200

    Producers - missing App + ProducerListItem classes

---
+++ TKproject/src/pl/tokajiwines/App.java
@@ -0,0 +1,19 @@
+package pl.tokajiwines;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class App {
+    
+    public static boolean isOnline(Context context) {
+        ConnectivityManager cm =
+            (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ TKproject/src/pl/tokajiwines/models/ProducerListItem.java
@@ -0,0 +1,17 @@
+package pl.tokajiwines.models;
+
+public class ProducerListItem {
+    
+    public int mIdProducer;
+    public String mName;
+    public String mDescription;
+    
+    public ProducerListItem(int id, String name, String description)
+    {
+        mIdProducer = id;
+        mName = name;
+        mDescription = description;
+    }
+    
+
+}
