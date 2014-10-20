34a55ed571487345ae1f78ad3f0eb3d2b95ac17b
paulina00 <konieczna.paulina00@gmail.com>
2014-10-20 19:57:26 +0200

    Producers...

--- TKproject/src/pl/tokajiwines/adapters/ProducersAdapter.java
+++ TKproject/src/pl/tokajiwines/adapters/ProducersAdapter.java
@@ -1,92 +1,74 @@
 
 package pl.tokajiwines.adapters;
 
+import android.app.Activity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import pl.tokajiwines.R;
+import pl.tokajiwines.models.ProducerListItem;
 
 public class ProducersAdapter extends BaseAdapter {
-    //wypierdolic
-    @Override
-    public int getCount() {
-        // TODO Auto-generated method stub
-        return 0;
+
+    Activity mActivity;
+    private static LayoutInflater inflater = null;
+    private ProducerListItem[] mProducers = null;
+
+    public ProducersAdapter(Activity act, ProducerListItem[] producers) {
+        mActivity = act;
+        inflater = (LayoutInflater) mActivity.getSystemService(mActivity.LAYOUT_INFLATER_SERVICE);
+        mProducers = producers;
+    }
+
+    public class Holder {
+        TextView title;
+        TextView content;
+        ImageView img;
     }
 
     @Override
     public Object getItem(int position) {
-        // TODO Auto-generated method stub
+        // TODO zrobic
         return null;
+    }
+
+    @Override
+    public int getCount() {
+        // TODO po pobraniu
+        return mProducers.length;
     }
 
     @Override
     public long getItemId(int position) {
-        // TODO Auto-generated method stub
+        // TODO zrobic
         return 0;
     }
 
+    //TODO 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        // TODO Auto-generated method stub
-        return null;
+        Holder holder = new Holder();
+        View rowView;
+        rowView = inflater.inflate(R.layout.item_vineyard, null);
+        holder.title = (TextView) rowView.findViewById(R.id.item_wineyard_title);
+        holder.content = (TextView) rowView.findViewById(R.id.item_wineyard_content);
+        holder.img = (ImageView) rowView.findViewById(R.id.item_wineyard_image);
+        holder.title.setText(mProducers[position].mName);
+        holder.img.setImageResource(R.drawable.ic_launcher);
+        holder.content.setText(mProducers[position].mDescription);
+        //        rowView.setOnClickListener(new OnClickListener() {
+        //            @Override
+        //            public void onClick(View v) {
+        //                // TODO Auto-generated method stub
+        //                Toast.makeText(context, "You Clicked " + result[position], Toast.LENGTH_LONG)
+        //                        .show();
+        //            }
+        //        });
+        return rowView;
     }
-    /*
-        Activity mActivity;
-        private static LayoutInflater inflater = null;
-        private ProducerListItem[] mProducers = null;
-
-        public ProducersAdapter(Activity act, ProducerListItem[] producers) {
-            mActivity = act;
-            inflater = (LayoutInflater) mActivity.getSystemService(mActivity.LAYOUT_INFLATER_SERVICE);
-            mProducers = producers;
-        }
-
-        public class Holder {
-            TextView title;
-            TextView content;
-            ImageView img;
-        }
-
-        @Override
-        public Object getItem(int position) {
-            // TODO zrobic
-            return null;
-        }
-
-        @Override
-        public int getCount() {
-            // TODO po pobraniu
-            return mProducers.length;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            // TODO zrobic
-            return 0;
-        }
-
-        //TODO 
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            Holder holder = new Holder();
-            View rowView;
-            rowView = inflater.inflate(R.layout.item_vineyard, null);
-            holder.title = (TextView) rowView.findViewById(R.id.item_wineyard_title);
-            holder.content = (TextView) rowView.findViewById(R.id.item_wineyard_content);
-            holder.img = (ImageView) rowView.findViewById(R.id.item_wineyard_image);
-            holder.title.setText(mProducers[position].mName);
-            holder.img.setImageResource(R.drawable.ic_launcher);
-            holder.content.setText(mProducers[position].mDescription);
-            //        rowView.setOnClickListener(new OnClickListener() {
-            //            @Override
-            //            public void onClick(View v) {
-            //                // TODO Auto-generated method stub
-            //                Toast.makeText(context, "You Clicked " + result[position], Toast.LENGTH_LONG)
-            //                        .show();
-            //            }
-            //        });
-            return rowView;
-        }
-        */
 
 }
--- TKproject/src/pl/tokajiwines/fragments/ProducersFragment.java
+++ TKproject/src/pl/tokajiwines/fragments/ProducersFragment.java
@@ -1,119 +1,139 @@
 
 package pl.tokajiwines.fragments;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import pl.tokajiwines.App;
+import pl.tokajiwines.R;
+import pl.tokajiwines.adapters.ProducersAdapter;
+import pl.tokajiwines.utils.JSONParser;
+import pl.tokajiwines.utils.Constans;
+import pl.tokajiwines.models.ProducerListItem;
+
 
 public class ProducersFragment extends BaseFragment {
-    /*
-        ListView mUiList;
-        ProducersAdapter mAdapter;
-        JSONParser mParser;
-        ProgressDialog mProgDial;
-        Context mContext;
-        private static final String sUrl = "http://remzo.usermd.net/zpi/producers.php";
-        private static final String TAG_ID = "idProducer";
-        private static final String TAG_NAME = "name";
-        private static final String TAG_SHORT_MESSAGE = "short";
-        private static final String TAG_PRODUCERS ="producers";
-        private JSONArray mProducersJSON = null;
-        private ProducerListItem[] mProducersList;
 
-        public static ProducersFragment newInstance() {
-            ProducersFragment fragment = new ProducersFragment();
-            //        Bundle args = new Bundle();
-            //        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            //        fragment.setArguments(args);
-            return fragment;
-        }
+    ListView mUiList;
+    ProducersAdapter mAdapter;
+    JSONParser mParser;
+    ProgressDialog mProgDial;
+    Context mContext;
+    private static final String sUrl = "http://remzo.usermd.net/zpi/producers.php";
+    private static final String TAG_ID = "idProducer";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_SHORT_MESSAGE = "short";
+    private static final String TAG_PRODUCERS ="producers";
+    private JSONArray mProducersJSON = null;
+    private ProducerListItem[] mProducersList;
 
-        public ProducersFragment() {
-        }
+    public static ProducersFragment newInstance() {
+        ProducersFragment fragment = new ProducersFragment();
+        //        Bundle args = new Bundle();
+        //        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        //        fragment.setArguments(args);
+        return fragment;
+    }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_news, container, false);
-            mUiList = (ListView) rootView.findViewById(R.id.frag_news_list);
-            mContext = getActivity();
+    public ProducersFragment() {
+    }
 
-            return rootView;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_news, container, false);
+        mUiList = (ListView) rootView.findViewById(R.id.frag_news_list);
+        mContext = getActivity();
+
+        return rootView;
+    }
+    
+    public void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+        
+        if (App.isOnline(mContext))
+        {
+            new ConnectingToDatabaseTask().execute();
         }
         
-        public void onResume() {
-            // TODO Auto-generated method stub
-            super.onResume();
-            
-            if (App.isOnline(mContext))
-            {
-                new ConnectingToDatabaseTask().execute();
-            }
-            
-            else
-            {
-                Toast.makeText(mContext, "Cannot connect to the Internet", Toast.LENGTH_LONG).show();
-            }
-
+        else
+        {
+            Toast.makeText(mContext, "Cannot connect to the Internet", Toast.LENGTH_LONG).show();
         }
 
-        
-        class ConnectingToDatabaseTask extends AsyncTask<String, String, String> {
+    }
+
+    
+    class ConnectingToDatabaseTask extends AsyncTask<String, String, String> {
 
 
-           boolean failure = false;
+       boolean failure = false;
 
-           @Override
-           protected void onPreExecute() {
-               super.onPreExecute();
-               mProgDial = new ProgressDialog(mContext);
-               mProgDial.setMessage("Loading producers data...");
-               mProgDial.setIndeterminate(false);
-               mProgDial.setCancelable(true);
-               mProgDial.show();
+       @Override
+       protected void onPreExecute() {
+           super.onPreExecute();
+           mProgDial = new ProgressDialog(mContext);
+           mProgDial.setMessage("Loading producers data...");
+           mProgDial.setIndeterminate(false);
+           mProgDial.setCancelable(true);
+           mProgDial.show();
 
-           }
+       }
 
-           @Override
-           protected String doInBackground(String... args) {
-               // TODO Auto-generated method stub
+       @Override
+       protected String doInBackground(String... args) {
+           // TODO Auto-generated method stub
+           
+           mParser = new JSONParser();
+           JSONObject json = mParser.getJSONFromUrl(sUrl, Constans.sUsername, Constans.sPassword);
+           
+           
+           try
+           {
+               mProducersJSON = json.getJSONArray(TAG_PRODUCERS);
                
-               mParser = new JSONParser();
-               JSONObject json = mParser.getJSONFromUrl(sUrl, Constans.sUsername, Constans.sPassword);
+               mProducersList = new ProducerListItem[mProducersJSON.length()];
                
+               System.out.println(mProducersJSON.length());
                
-               try
+               for (int i = 0; i < mProducersJSON.length(); i++)
                {
-                   mProducersJSON = json.getJSONArray(TAG_PRODUCERS);
+                   JSONObject  p = mProducersJSON.getJSONObject(i);
+                   mProducersList[i] = new ProducerListItem(p.getInt(TAG_ID), p.getString(TAG_NAME), p.getString(TAG_SHORT_MESSAGE));
                    
-                   mProducersList = new ProducerListItem[mProducersJSON.length()];
-                   
-                   System.out.println(mProducersJSON.length());
-                   
-                   for (int i = 0; i < mProducersJSON.length(); i++)
-                   {
-                       JSONObject  p = mProducersJSON.getJSONObject(i);
-                       mProducersList[i] = new ProducerListItem(p.getInt(TAG_ID), p.getString(TAG_NAME), p.getString(TAG_SHORT_MESSAGE));
-                       
-                   }
                }
-               
-               catch(JSONException e)
-               {
-                   e.printStackTrace();
-               }
-
-
-               return null;
-
+           }
+           
+           catch(JSONException e)
+           {
+               e.printStackTrace();
            }
 
-           protected void onPostExecute(String file_url) {
-               
-               super.onPostExecute(file_url);
-               mProgDial.dismiss();
-               mAdapter = new ProducersAdapter(getActivity(),mProducersList);
-               mUiList.setAdapter(mAdapter);
-        
-           }
-     
 
-           }
-           */
+           return null;
+
+       }
+
+       protected void onPostExecute(String file_url) {
+           
+           super.onPostExecute(file_url);
+           mProgDial.dismiss();
+           mAdapter = new ProducersAdapter(getActivity(),mProducersList);
+           mUiList.setAdapter(mAdapter);
+    
+       }
+ 
+
+       }
 }
