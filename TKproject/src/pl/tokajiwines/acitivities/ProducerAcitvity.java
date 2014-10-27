2835fad902fa1556b57042a580bbdad5a661b459
PSyron <paul.cyron1@gmail.com>
2014-10-27 14:50:44 +0100

    Producers not working because Wojtek ssie pale

--- TKproject/src/pl/tokajiwines/acitivities/ProducerAcitvity.java
+++ TKproject/src/pl/tokajiwines/acitivities/ProducerAcitvity.java
@@ -1,20 +1,34 @@
 
 package pl.tokajiwines.acitivities;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
+
+import pl.tokajiwines.App;
 import pl.tokajiwines.R;
 import pl.tokajiwines.fragments.ProducersFragment;
+import pl.tokajiwines.models.Producer;
 import pl.tokajiwines.models.ProducerListItem;
+import pl.tokajiwines.utils.Constans;
+import pl.tokajiwines.utils.JSONParser;
 import pl.tokajiwines.utils.Log;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class ProducerAcitvity extends BaseActivity {
-
+    JSONParser mParser;
     public static int REQUEST = 997;
-
+    private static final String sUrl = "http://remzo.usermd.net/zpi/services/producerDetails.php";
+    ProgressDialog mProgDial;
     ProducerListItem mProducer;
+    Producer mProducerFromBase;
 
     TextView mUiTitle;
     ImageView mUiImage;
@@ -22,8 +36,10 @@ public class ProducerAcitvity extends BaseActivity {
     TextView mUiOpeningHours;
     TextView mUiPhoneNumber;
     TextView mUiUrl;
+    TextView mUiDescription;
     ImageView mUiNear;
     ImageView mUiNavigate;
+    TextView mUiMoreWines;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +51,12 @@ public class ProducerAcitvity extends BaseActivity {
         }
         Log.e(ProducerAcitvity.class.getName(), mProducer + " ");
         initView();
-        fillView();
 
     }
 
     public void initView() {
+        getActionBar().setHomeButtonEnabled(true);
+        getActionBar().setTitle(mProducer.mName);
         mUiTitle = (TextView) findViewById(R.id.activity_news_details_name);
         mUiImage = (ImageView) findViewById(R.id.activity_news_details_image);
         mUiAddress = (TextView) findViewById(R.id.activity_producer_details_adress);
@@ -48,12 +65,86 @@ public class ProducerAcitvity extends BaseActivity {
         mUiUrl = (TextView) findViewById(R.id.activity_producer_details_url);
         mUiNear = (ImageView) findViewById(R.id.activity_producer_navigate);
         mUiNavigate = (ImageView) findViewById(R.id.activity_producer_neighborhood);
+        mUiMoreWines = (TextView) findViewById(R.id.activity_producer_details_wine_button);
+        mUiDescription = (TextView) findViewById(R.id.activity_news_details_description);
     }
 
     public void fillView() {
-        getActionBar().setHomeButtonEnabled(true);
-        getActionBar().setTitle(mProducer.mName);
-        mUiTitle.setText(mProducer.mName);
+
+        mUiTitle.setText(mProducerFromBase.mName);
+    }
+
+    public void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+
+        // if there is an access to the Internet, try to load data from remote database
+
+        if (App.isOnline(ProducerAcitvity.this)) {
+            new LoadProducerTask().execute();
+        }
+
+        // otherwise, show message
+
+        else {
+            Toast.makeText(ProducerAcitvity.this, "Cannot connect to the Internet",
+                    Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    class LoadProducerTask extends AsyncTask<Void, String, String> {
+
+        boolean failure = false;
+
+        // while data are loading, show progress dialog
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgDial = new ProgressDialog(ProducerAcitvity.this);
+            mProgDial.setMessage("Loading producer details...");
+            mProgDial.setIndeterminate(false);
+            mProgDial.setCancelable(true);
+            // mProgDial.show();
+
+        }
+
+        // retrieving producers data
+
+        @Override
+        protected String doInBackground(Void... args) {
+
+            mParser = new JSONParser();
+            String tempUrl = sUrl + "?who=" + mProducer.mIdProducer;
+
+            InputStream source = mParser.retrieveStream(tempUrl, Constans.sUsername,
+                    Constans.sPassword, null);
+            Gson gson = new Gson();
+            InputStreamReader reader = new InputStreamReader(source);
+
+            Producer response = gson.fromJson(reader, Producer.class);
+
+            if (response != null) {
+                Log.e(ProducerAcitvity.class.getName(), response.mName);
+                mProducerFromBase = response;
+            }
+
+            return null;
+
+        }
+
+        // create adapter that contains loaded data and show list of producers
+
+        protected void onPostExecute(String file_url) {
+
+            super.onPostExecute(file_url);
+            mProgDial.dismiss();
+            if (mProducerFromBase != null) {
+                fillView();
+            }
+        }
+
     }
 
 }
