62ccce1491f67089bc900ae89376590e748af60c
Nietoperek <boberjunior@gmail.com>
2014-10-28 23:28:55 +0100

    News dla Pauliny

---
+++ TKproject/src/pl/tokajiwines/models/NewsPauliny.java
@@ -0,0 +1,31 @@
+
+package pl.tokajiwines.models;
+
+import java.io.Serializable;
+
+public class NewsPauliny implements Serializable {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -2308726436283474494L;
+
+    public String header;
+    public String vast;
+    public String entryDate;
+    public String startDate;
+    public String endDate;
+    public String image;
+
+    /*    public News(String t, String c) {
+            Title = t;
+            Content = c;
+        }
+
+        public String getTitle() {
+            return this.Title;
+
+        public String getContent() {
+            return this.Content;
+        }*/
+}
