da2449e8a5ccad2f8f9cbd30bc9aa8e2990f442e
paulina00 <konieczna.paulina00@gmail.com>
2014-11-05 00:09:12 +0100

    Wine filter

---
+++ TKproject/src/pl/tokajiwines/jsonresponses/WineFilterResponse.java
@@ -0,0 +1,12 @@
+package pl.tokajiwines.jsonresponses;
+
+public class WineFilterResponse {
+    
+    public int success;
+    public String message;
+    public String[] years;
+    public String[] strains;
+    public String[] grades;
+    public String[] producers;
+
+}
